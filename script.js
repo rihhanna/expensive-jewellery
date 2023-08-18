b1d894c0213ef9988f1feb31cf81e5197aef2022
rihhana <hrihhana@gmail.com>
2023-08-18 20:32:55 +0300

    answers of expensive jewellery

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // Good Luck 💪🏾
+function mostExpensiveItemName(sampleCart) {
+    let expensive = 0;
+    let name;
+    for (let i = 0; i < sampleCart.length; i++) {
+      if (sampleCart[i].price > expensive) {
+        expensive = sampleCart[i].price;
+        name = sampleCart[i].itemName;
+      }
+    }
+    return `The most expensive one is the ${name}`;
+  }
+  const sampleCart = [
+    { itemName: "Diamond Earrings",price: 980 },
+    { itemName: "Gold watch", price: 250 },
+    { itemName: "Pearl Necklace", price: 4650 },
+  ];
+  
+  console.log(mostExpensiveItemName(sampleCart)); // The most expensive one is the Pearl Necklace
+  
